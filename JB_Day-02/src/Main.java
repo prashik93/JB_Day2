30ca11a32e860595d7a320cb80c4ee24fe018b5b
Prashik Kamble <kambleprashik7349@gmail.com>
2022-11-09 11:19:02 +0530

    ADDED : code for reverse a number

--- JB_Day-02/src/Main.java
+++ JB_Day-02/src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         WhileLoop whileObj = new WhileLoop();
         whileObj.sumOfNaturalNumbers();
+        whileObj.reverseNumber();
 
 //        singleDigit(args);
 //        displayUnit(args);
--- JB_Day-02/src/WhileLoop.java
+++ JB_Day-02/src/WhileLoop.java
@@ -20,4 +20,17 @@ public class WhileLoop {
         }
         System.out.println("Sum of n Natural numbers is : " + sum);
     }
+
+    public void reverseNumber() {
+        System.out.println("Enter a number : ");
+        num = scnr.nextInt();
+        int rem = 0;
+        int rev = 0;
+        while (num > 0) {
+            rem = num % 10;
+            rev = (rev * 10) + rem;
+            num = num / 10;
+        }
+        System.out.println("Reverse of a : " + num + "is : " + rev);
+    }
 }
