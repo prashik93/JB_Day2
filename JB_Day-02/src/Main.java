68515ff2b55a4d77e789d04a4e39953ac70f66d3
Prashik Kamble <kambleprashik7349@gmail.com>
2022-11-09 10:06:50 +0530

    ADDED : code for read a single digit number

---
+++ JB_Day-02/src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+
+        if (args[0].equals("0")) {
+            System.out.println("Zero");
+        } else if (args[0].equals("1")) {
+            System.out.println("One");
+        } else if (args[0].equals("2")) {
+            System.out.println("Two");
+        } else if (args[0].equals("3")) {
+            System.out.println("Three");
+        } else if (args[0].equals("4")) {
+            System.out.println("Four");
+        } else if (args[0].equals("5")) {
+            System.out.println("Five");
+        } else if (args[0].equals("6")) {
+            System.out.println("Six");
+        } else if (args[0].equals("7")) {
+            System.out.println("Seven");
+        } else if (args[0].equals("8")) {
+            System.out.println("Eight");
+        } else if (args[0].equals("9")) {
+            System.out.println("Nine");
+        } else {
+            System.out.println("Number is not a single digit number");
+        }
+    }
+}
